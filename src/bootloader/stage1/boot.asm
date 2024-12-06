2196a28b3e16e9d0df38cf085b869066ce239fc8
Janis Gaetzi <janis.gaetzi@icloud.com>
2024-12-06 22:30:56 +0100

    error handling

--- src/bootloader/stage1/boot.asm
+++ src/bootloader/stage1/boot.asm
@@ -364,7 +364,7 @@ msg_loading:            db 'Loading...', ENDL, 0
 msg_read_failed:        db 'Read from disk failed!', ENDL, 0
 msg_stage2_not_found:   db 'STAGE2.BIN file not found!', ENDL, 0
 file_stage2_bin:        db 'STAGE2  BIN'
-stage2_cluster:         dw 0
+stage2_cluster:         dw 0 
 
 STAGE2_LOAD_SEGMENT     equ 0x0
 STAGE2_LOAD_OFFSET      equ 0x500
